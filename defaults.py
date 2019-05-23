1b2873a02c743a3f766c9289a9da94fa10c0168a
Toby Evans <tobz.nz@gmail.com>
2019-05-23 21:51:46 +1200

    Update default settings with some LTE configs

--- defaults.py
+++ defaults.py
@@ -1,3 +1,10 @@
 last_level_file = '/flash/last_level.py'
 
+level_threshhold = 5
+
 sleep_for = 300
+
+lte_apn = 'm2m'
+
+# https://support.m2mone.co.nz/portal/kb/articles/spark-operating-frequencies
+lte_band = 3
