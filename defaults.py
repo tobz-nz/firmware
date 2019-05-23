ec7deeb615efbc690dc065d79177f9f076a549c3
Toby Evans <tobz.nz@gmail.com>
2019-05-23 23:12:03 +1200

    Some more defaults

--- defaults.py
+++ defaults.py
@@ -1,10 +1,20 @@
-last_level_file = '/flash/last_level.py'
+from binascii import hexlify
+from machine import unique_id
 
+# Level
+last_level_file = '/flash/last_level.py'
 level_threshhold = 5
 
-sleep_for = 300
-
+# LTE
 lte_apn = 'm2m'
 
 # https://support.m2mone.co.nz/portal/kb/articles/spark-operating-frequencies
 lte_band = 3
+
+# WIFI
+wlan_ssid = 'Monkeys'
+wlan_pass = 'conspire-la-skeet-roust'
+
+# System
+uid = hexlify(unique_id()).decode()
+sleep_for = 300
