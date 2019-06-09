d2b428d68f725a4fd098a01d178a6451770659c2
Toby Evans <tobz.nz@gmail.com>
2019-06-09 18:42:52 +1200

    Update main
    Mostly catch any unhandled errors and log them to a file.

--- main.py
+++ main.py
@@ -1,55 +1,65 @@
 import defaults, level, tankful
 from machine import deepsleep
 
-# load last reading
-last_level = level.last()
+try:
 
-# Take a new reading
-current_level = level.get()
+    # load last reading
+    last_level = level.last()
 
-print('Levels: {previous} / {current}'.format(previous=last_level, current=current_level))
+    # Take a new reading
+    current_level = level.get()
 
-# check if reading needs to be be sent to server
-diff = abs(last_level - current_level)
-if diff > defaults.level_threshhold:
-    # yes it does
-    import net, power
+    print('Levels: {previous} / {current}'.format(previous=last_level, current=current_level))
 
-    # connect to network
-    connection = net.connect()
-    if connection:
-        # send data
-        tankful.post('devices/%s/metrics' % defaults.uid, {
-            'level': current_level,
-            'battery': power.level(),
-            'charging': power.is_charging()
-        })
-        # - reading value
-        # - battery level
-        # - battery charging status
+    # check if reading needs to be be sent to server
+    diff = abs(last_level - current_level)
+    if diff > defaults.level_threshhold:
+        # yes it does
+        import net, power
 
-            # - crital logs
-            # - other logs if in debug mode
-            # - previously failed readings
-            # success
-                # clear previsouly failed readings that succeeded this time
-                # check if response has new update header/data
-                    # run update process
-            # error
-                # store reading as unsent - to attempt send again next time
+        # connect to network
+        connection = net.connect()
+        if connection:
+            # send data
+            tankful.post('devices/%s/metrics' % defaults.uid, {
+                'level': current_level,
+                'battery': power.level(),
+                'charging': power.is_charging()
+            })
+            # - reading value
+            # - battery level
+            # - battery charging status
 
+                # - crital logs
+                # - other logs if in debug mode
+                # - previously failed readings
+                # success
+                    # clear previsouly failed readings that succeeded this time
+                    # check if response has new update header/data
+                        # run update process
+                # error
+                    # store reading as unsent - to attempt send again next time
+
+            net.disconnect(connection)
+        else:
+            print('Failed to connect to network')
+            print('Storing level and will attempt to send again later')
+
+                # store as last reading
+        # no it doesn't
+            # sleep
+    elif tankful.should_ping():
+        connection = net.connect()
+        tankful.ping()
         net.disconnect(connection)
-    else:
-        print('Failed to connect to network')
-        print('Storing level and will attempt to send again later')
 
-            # store as last reading
-    # no it doesn't
-        # sleep
-elif tankful.should_ping():
-    connection = net.connect()
-    tankful.ping()
-    net.disconnect(connection)
+except Exception as err:
+    # log error
+    print(err.args[0])
+    file = open('/flash/error.log', 'a')
+    file.write(err.args[0])
+    file.close()
 
-print('Sleeping for %s' % defaults.sleep_for)
-# deepsleep(defaults.sleep_for)
+finally:
+    print('Sleeping for %s' % defaults.sleep_for)
+    # deepsleep(defaults.sleep_for)
