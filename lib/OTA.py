82c78d8d26c64a65bff7403c0a975505613cce5e
Toby Evans <tobz.nz@gmail.com>
2019-08-16 20:46:55 +1200

    Work on OTA

--- lib/OTA.py
+++ lib/OTA.py
@@ -11,7 +11,7 @@
 import network
 import socket
 import machine
-import ujson
+import json
 import uhashlib
 import ubinascii
 import gc
@@ -19,8 +19,9 @@ import pycom
 import os
 import machine
 
-import defaults, tankful
-from http import MicroWebCli as HTTP
+import defaults
+import tankful
+import urequests as requests
 
 # Try to get version number
 try:
@@ -37,13 +38,15 @@ class OTA():
         tankful.connect()
 
     def get_data(self, req, dest_path=None):
-        response = HTTP.GETRequest(self.url(req))
+        response = requests.get(self.url(req))
 
-        if type(response) is HTTP._response:
+        if response is not None:
             if dest_path is not None:
-                reponse.WriteContentToFile(dest_path)
+                file = open(dest_path, 'w')
+                file.write(response.text)
+                file.close()
 
-            file_content = response.ReadContent()
+            file_content = response.text
 
             hash = uhashlib.sha1()
             hash.update(file_content)
@@ -53,7 +56,7 @@ class OTA():
             return file_content, file_hash
 
 
-    def url(uri):
+    def url(self, uri):
         import re
         uri = '/' + re.match(r'^\/?(.+)', uri).group(1)
 
@@ -66,48 +69,74 @@ class OTA():
 
     def get_update_manifest(self):
         req = "/manifest.json?current_ver={}".format(self.get_current_version())
-        response = HTTP.JSONRequest(self.url(req))
-        manifest = response.ReadContentAsJSON()
+        print(req)
+        response = requests.get(
+            self.url('/manifest.json'),
+            params={'current_ver':self.get_current_version()}
+        )
+
+        try:
+            manifest = json.loads(response.text)
+        except:
+            manifest = None
+            pass
 
         gc.collect()
-        return manifest
+        return manifest, response
 
     def update(self):
-        manifest = self.get_update_manifest()
+        manifest, response = self.get_update_manifest()
+
+        print(manifest)
+
         if manifest is None:
             print("Already on the latest version")
-            return
+            return response
 
-        # Download new files and verify hashes
-        for f in manifest['new'] + manifest['update']:
-            # Upto 5 retries
-            for _ in range(5):
-                try:
-                    self.get_file(f)
-                    break
-                except Exception as e:
-                    print(e)
-                    msg = "Error downloading `{}` retrying..."
-                    print(msg.format(f['URL']))
-            else:
-                raise Exception("Failed to download `{}`".format(f['URL']))
+        if 'current_ver' in manifest:
+            print(manifest['current_ver'][0])
+            return response
 
-        # Backup old files
-        # only once all files have been successfully downloaded
-        for f in manifest['update']:
-            self.backup_file(f)
+        if 'new' in manifest and 'update' in manifest:
+            # Download new files and verify hashes
+            for f in manifest['new'] + manifest['update']:
+                # Upto 5 retries
+                for _ in range(5):
+                    try:
+                        self.get_file(f)
+                        break
+                    except Exception as e:
+                        print(e)
+                        msg = "Error downloading `{}` retrying..."
+                        print(msg.format(f['URL']))
+                else:
+                    raise Exception("Failed to download `{}`".format(f['URL']))
 
-        # Rename new files to proper name
-        for f in manifest['new'] + manifest['update']:
-            new_path = "{}.new".format(f['dst_path'])
-            dest_path = "{}".format(f['dst_path'])
+            # Backup old files
+            # only once all files have been successfully downloaded
+            for f in manifest['update']:
+                self.backup_file(f)
 
-            os.rename(new_path, dest_path)
+            # Rename new files to proper name
+            for f in manifest['new'] + manifest['update']:
+                new_path = "{}.new".format(f['dest_path'])
+                dest_path = "{}".format(f['dest_path'])
 
-        # `Delete` files no longer required
-        # This actually makes a backup of the files incase we need to roll back
-        for f in manifest['delete']:
-            self.delete_file(f)
+                # make sure desitnation path is free first
+                self.delete_file(dest_path)
+
+                # rename
+                os.rename(new_path, dest_path)
+
+        else:
+            print('invalid manifest')
+            print(manifest)
+
+        if 'delete' in manifest:
+            # `Delete` files no longer required
+            # This actually makes a backup of the files incase we need to roll back
+            for f in manifest['delete']:
+                self.delete_file(f)
 
         # Flash firmware
         if "firmware" in manifest:
@@ -115,18 +144,20 @@ class OTA():
 
         # Save version number
         try:
-            self.backup_file({"dst_path": "/flash/OTA_VERSION.py"})
+            self.backup_file({"dest_path": "/flash/OTA_VERSION.py"})
         except OSError:
             pass  # There isnt a previous file to backup
+
         with open("/flash/OTA_VERSION.py", 'w') as fp:
             fp.write("VERSION = '{}'".format(manifest['version']))
+
         from OTA_VERSION import VERSION
 
         # Reboot the device to run the new decode
         machine.reset()
 
     def get_file(self, f):
-        new_path = "{}.new".format(f['dst_path'])
+        new_path = "{}.new".format(f['dest_path'])
 
         # If a .new file exists from a previously failed update delete it
         try:
@@ -140,13 +171,14 @@ class OTA():
 
         # Hash mismatch
         if hash != f['hash']:
-            print(hash, f['hash'])
             msg = "Downloaded file's hash does not match expected hash"
+            print(hash)
+            print(f['hash'])
             raise Exception(msg)
 
     def backup_file(self, f):
-        bak_path = "{}.bak".format(f['dst_path'])
-        dest_path = "{}".format(f['dst_path'])
+        bak_path = "{}.bak".format(f['dest_path'])
+        dest_path = "{}".format(f['dest_path'])
 
         # Delete previous backup if it exists
         try:
@@ -173,125 +205,3 @@ class OTA():
     def write_firmware(self, f):
         hash = self.get_data(f['URL'].split("/", 3)[-1])
         # TODO: Add verification when released in future firmware
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class WiFiOTA(OTA):
-    def __init__(self, ssid, password, ip, port):
-        self.SSID = ssid
-        self.password = password
-        self.ip = ip
-        self.port = port
-
-    def connect(self):
-        self.wlan = network.WLAN(mode=network.WLAN.STA)
-        if not self.wlan.isconnected() or self.wlan.ssid() != self.SSID:
-            for net in self.wlan.scan():
-                if net.ssid == self.SSID:
-                    self.wlan.connect(self.SSID, auth=(network.WLAN.WPA2,
-                                                       self.password))
-                    while not self.wlan.isconnected():
-                        machine.idle()  # save power while waiting
-                    break
-            else:
-                raise Exception("Cannot find network '{}'".format(SSID))
-        else:
-            # Already connected to the correct WiFi
-            pass
-
-    def _http_get(self, path, host):
-        req_fmt = 'GET /{} HTTP/1.0\r\nHost: {}\r\n\r\n'
-        req = bytes(req_fmt.format(path, host), 'utf8')
-        return req
-
-    def get_data(self, req, dest_path=None, hash=False, firmware=False):
-        h = None
-
-        # Connect to server
-        print("Requesting: {}".format(req))
-        s = socket.socket(socket.AF_INET,
-                          socket.SOCK_STREAM,
-                          socket.IPPROTO_TCP)
-        s.connect((self.ip, self.port))
-
-        # Request File
-        s.sendall(self._http_get(req, "{}:{}".format(self.ip, self.port)))
-
-        try:
-            content = bytearray()
-            fp = None
-            if dest_path is not None:
-                if firmware:
-                    raise Exception("Cannot write firmware to a file")
-                fp = open(dest_path, 'wb')
-
-            if firmware:
-                pycom.ota_start()
-
-            h = uhashlib.sha1()
-
-            # Get data from server
-            result = s.recv(100)
-
-            start_writing = False
-            while (len(result) > 0):
-                # Ignore the HTTP headers
-                if not start_writing:
-                    if "\r\n\r\n" in result:
-                        start_writing = True
-                        result = result.decode().split("\r\n\r\n")[1].encode()
-
-                if start_writing:
-                    if firmware:
-                        pycom.ota_write(result)
-                    elif fp is None:
-                        content.extend(result)
-                    else:
-                        fp.write(result)
-
-                    if hash:
-                        h.update(result)
-
-                result = s.recv(100)
-
-            s.close()
-
-            if fp is not None:
-                fp.close()
-            if firmware:
-                pycom.ota_finish()
-
-        except Exception as e:
-            # Since only one hash operation is allowed at Once
-            # ensure we close it if there is an error
-            if h is not None:
-                h.digest()
-            raise e
-
-        hash_val = ubinascii.hexlify(h.digest()).decode()
-
-        if dest_path is None:
-            if hash:
-                return (bytes(content), hash_val)
-            else:
-                return bytes(content)
-        elif hash:
-            return hash_val
